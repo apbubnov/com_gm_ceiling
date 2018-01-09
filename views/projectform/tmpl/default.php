40776edd3422dfd8057ba6a27da8fb38a29964bc
Наталия В. Таранцева <tarantsevanv@gm.local>
2018-01-09 14:59:40 +0300

    исправила, убала консоль

--- views/projectform/tmpl/default.php
+++ views/projectform/tmpl/default.php
@@ -1468,7 +1468,6 @@ echo parent::getPreloader();
                         jQuery("#projects_brigade_container").empty();
                         var table_projects = '<p style="margin-top: 1em; margin-bottom: 0;"><strong>Монтажи бригады:</strong></p><table id="projects_brigade">';
                         table_projects += '<tr class="caption"><td>Время</td><td>Адрес</td><td>Периметр</td></tr>';
-                        console.log(data);
                         Array.from(data).forEach(function(element) {
                             if (element.project_mounter == selectedBrigade) {
                                 if (element.project_mounting_day_off != "") {
