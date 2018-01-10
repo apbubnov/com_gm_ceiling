6962f74838af48cc47a3a4e873898ed784aaadbe
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-10 11:36:05 +0300

    изменение n3 при раскрое (исправление ошибки) 5

--- models/calculations.php
+++ models/calculations.php
@@ -1055,7 +1055,7 @@ class Gm_ceilingModelCalculations extends JModelList {
             $query = $db->getQuery(true);
             $query->select("id")
             ->from('#__gm_ceiling_canvases')
-            ->where('texture_id = '. $item->n3 . ' and name = '.  $db->quote($item_canvas->name) . ' and country = ' . $db->quote($item_canvas->country) .'and width = '.  $db->quote($width));    
+            ->where('texture_id = '. $item->n2 . ' and name = '.  $db->quote($item_canvas->name) . ' and country = ' . $db->quote($item_canvas->country) .'and width = '.  $db->quote($width));    
             if(!empty($item_canvas->color_id))  $query->where('color_id = '. $item_canvas->color_id); 
             $db->setQuery($query);
             $new_n3  = $db->loadObject();
