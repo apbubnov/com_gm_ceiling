3739136d3e1803eacddb6001971dd7fb97215ba9
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-10 16:12:46 +0300

    тоже

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -4765,7 +4765,7 @@ class Gm_ceilingHelpersGm_ceiling
                             $statusDayOff[substr($value->date_from, 8, 2)] = "DayOff";
                         }
                     }
-                    throw new Exception($AllDayOff);
+                    throw new Exception(count($AllDayOff));
                     
                     // заполнение дней
                     if ($DayMounter[$j - $first_day_of_week + 1][0] == "red") {
