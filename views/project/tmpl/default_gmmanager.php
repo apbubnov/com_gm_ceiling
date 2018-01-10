317c7b6888bed98c3766d6d3b8b13ebc75f01624
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-10 15:16:23 +0300

    тоже самое

--- views/project/tmpl/default_gmmanager.php
+++ views/project/tmpl/default_gmmanager.php
@@ -740,6 +740,7 @@ $AllMounters = $model->FindAllMounters($where);
                             type: "success",
                             text: "Данные изменены"
                             });
+                            console.log(datetime.substr(5, 2));
                             update_calendar(datetime.substr(5, 2), datetime.substr(0, 4));
                             if (datetime.substr(5, 1) == "0") {
                                 if (datetime.substr(6, 1) == "9") {
@@ -758,7 +759,6 @@ $AllMounters = $model->FindAllMounters($where);
                             } else {
                                 year = datetime.substr(0, 4);
                             }
-                            console.log(month);
                             update_calendar2(month, year);
                         }
                     },
