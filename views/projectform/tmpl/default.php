85881b33841f5f388e0fc5cece022453298247c1
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-10 13:50:46 +0300

    исправление вывода даты

--- views/projectform/tmpl/default.php
+++ views/projectform/tmpl/default.php
@@ -1594,6 +1594,7 @@ echo parent::getPreloader();
 
             //если сессия есть, то выдать дату, которая записана в сессии
             var datesession = jQuery("#jform_project_mounting_date").val();
+            console.log(datesession);
             if (datesession != undefined) {
                 if (datesession.substr(8, 2) == "0") {
                     daytocalendar = datesession.substr(9, 1);
@@ -1605,6 +1606,10 @@ echo parent::getPreloader();
                 } else {
                     monthtocalendar = datesession.substr(5, 2);
                 }
+                console.log(datesession.substr(8, 2));
+                console.log(daytocalendar);
+                console.log(datesession.substr(5, 2));
+                console.log(monthtocalendar);
                 jQuery("#current-monthD"+daytocalendar+"DM"+monthtocalendar+"MY"+datesession.substr(0, 4)+"YI"+<?php echo $userId; ?>+"I").addClass("change");
             }
             //-----------------------------------------------------------
