9ea03f9c6f91533fdcb18a267c7a19391c90b423
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-11 15:45:42 +0300

    ывод замеров у НМС у замерщиков 25

--- views/gaugers/tmpl/default.php
+++ views/gaugers/tmpl/default.php
@@ -485,7 +485,7 @@ foreach ($gaugers_id as $value) {
 					},
 					success: function(data) {
 						//Вывод замеров у НМС у замерщиков 25
-						data = JSON.parse(data);
+						//data = JSON.parse(data);
 						console.log(data);
 						Array.from(data).forEach(function(element) {
 							table += '<tr><td style="width: 25%;">'+element.project_calculation_date.substr(11, 5)+" - "+element.project_calculation_day_off.substr(11, 5)+'</td><td style="width: 75%;">Выходной</td></tr>';
