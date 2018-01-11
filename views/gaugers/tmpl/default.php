5c650ff88ad0561170773300a2865c698d783eaf
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-11 16:26:21 +0300

    Вывод замеров у НМС у замерщиков 34

--- views/gaugers/tmpl/default.php
+++ views/gaugers/tmpl/default.php
@@ -524,7 +524,7 @@ foreach ($gaugers_id as $value) {
 										timegauging2++;
 									}
 									timegauging = element.project_calculation_date.substr(11, 5)+" - "+timegauging2+":00";
-									table += '<tr><td>'+timegauging+'</td><td>'+element.project_info+'</td></tr>';
+									table += '<tr><td style="width: 50%;">'+timegauging+'</td><td style="width: 50%;">'+element.project_info+'</td></tr>';
 								}
 							}
 						});
@@ -565,7 +565,7 @@ foreach ($gaugers_id as $value) {
 		});
 		// -----------------------------------------
 
-		//Вывод замеров у НМС у замерщиков 33
+		//Вывод замеров у НМС у замерщиков 34
 
         // получение значений из селектов
         jQuery("#modal-window-container-tar").on("click", "#save-choise-tar", function() {
