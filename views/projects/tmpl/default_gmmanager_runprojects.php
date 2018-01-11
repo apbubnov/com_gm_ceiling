c15ede960172c6c814f656ae3c415d870c60d22f
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-11 15:34:40 +0300

    вывод данных 10

--- views/projects/tmpl/default_gmmanager_runprojects.php
+++ views/projects/tmpl/default_gmmanager_runprojects.php
@@ -199,8 +199,8 @@ $model = Gm_ceilingHelpersGm_ceiling::getModel('calculations');
                 mouting_sum = jQuery(this).closest("tr").find("#mounting_sum").val();
                 material_sum = jQuery(this).closest("tr").find("#material_sum").val();
 				var subject = "Отметка стоимости договора №" + td.data("project_id"),
-                    text = "<p><input name='check_mount' onclick='changeDone(this,new_value,mouting_sum,material_sum);' class='radio' id ='done' value='1'  type='radio' checked><label for = 'done'>Монтаж выполнен</label></p>";
-					text += "<p><input name='check_mount' onclick='changeDone(this,new_value,mouting_sum,material_sum);'  class='radio' id ='not_done' value='0'  type='radio'><label for = 'not_done'>Монтаж недовыполнен</label></p>";
+                    text = "<p><input name='check_mount' onclick='changeDone(this,"+new_value+","+mouting_sum+","+material_sum+");' class='radio' id ='done' value='1'  type='radio' checked><label for = 'done'>Монтаж выполнен</label></p>";
+					text += "<p><input name='check_mount' onclick='changeDone(this,"+new_value+","+mouting_sum+","+material_sum+");'  class='radio' id ='not_done' value='0'  type='radio'><label for = 'not_done'>Монтаж недовыполнен</label></p>";
                     text += "<div class='dop_info'><div class='center'>Укажите новую стоимость договора</div><div class='center'><input id='input_check' class='noty_input' value='" + new_value + "'/></div></br>";
                     text += "<div class='center'>Укажите новую стоимость расходных материалов</div><div class='center'><input id='input_material' class='noty_input' value='" + material_sum + "'/></div></br>";
                     text += "<div class='center'>Укажите новую стоимость монтажных работ</div><div class='center'><input id='input_mounting' class='noty_input' value='" + mouting_sum + "'/></div>";
@@ -303,9 +303,7 @@ $model = Gm_ceilingHelpersGm_ceiling::getModel('calculations');
     function changeDone(element, new_value, mouting_sum, material_sum) {
         var text = "";
         element = $(element);
-        new_value = $(new_value);
-        mouting_sum = $(mouting_sum);
-        material_sum = $(material_sum);
+        alert(new_value);
             if ($("#done").is(":checked")) {
                 alert("allooo");
                 text += "<div class='center'>Укажите новую стоимость договора</div><div class='center'><input id='input_check' class='noty_input' value='" + new_value + "'/></div></br>";
