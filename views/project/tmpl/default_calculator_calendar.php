f73ba62e8173ffb0e8c0025e42e53ec2370b9c2b
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-12 14:36:26 +0300

    принятие данных на контроллер о отмене расходки 2

--- views/project/tmpl/default_calculator_calendar.php
+++ views/project/tmpl/default_calculator_calendar.php
@@ -982,7 +982,7 @@ $results = $db->loadObjectList();
             </tr>
             <tr>
                 <td>
-                    <input name='smeta' value='' type='checkbox'> Отменить смету по расходным материалам
+                    <input name='smeta' value='0' type='checkbox'> Отменить смету по расходным материалам
                 </td>
                 <td></td><td></td>
             </tr>
@@ -2029,6 +2029,12 @@ var $ = jQuery;
             calculate_total();
             calculate_total1();
             trans();
+        });
+
+        jQuery("input[name^='smeta']").click(function () {
+            if(jQuery("input[name^='smeta']").attr("checked") == 'checked')
+                jQuery("input[name='project_verdict']").val(1);
+            else jQuery("input[name='project_verdict']").val(0);
         });
 
         jQuery("#client_order").click(function () {
