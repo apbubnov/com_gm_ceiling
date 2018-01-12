c827c2e22fe571a0ea27eda5f0576664c9e84c4e
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-12 10:50:52 +0300

    пока без даты рождения

--- views/project/tmpl/default_calculator_calendar.php
+++ views/project/tmpl/default_calculator_calendar.php
@@ -305,12 +305,14 @@ $results = $db->loadObjectList();
                                 </div>
                             </td>
                         </tr>
+                        <!--
                         <tr>
                             <th>Дата рождения</th>
                             <td><input name="new_birthday" id="jform_birthday" class="inputactive"
                                         value="" placeholder="Дата рождения" type="date"></td>
                             <td><button type="button" class = "btn btn-primary" id = "add_birthday">Ок</button></td>
                         </tr>
+                        -->
                         <tr>
                             <th><?php echo JText::_('COM_GM_CEILING_CLIENTS_CLIENT_CONTACTS'); ?></th>
                             <?php $phone = $model->getClientPhones($this->item->id_client); ?>
