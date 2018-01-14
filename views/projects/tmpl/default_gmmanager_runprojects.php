433db7491930c810d58aa65678e8d6166dfc992a
Alina Popova <popowa.alinochka@gmail.com>
2018-01-14 23:17:30 +0300

    3

--- views/projects/tmpl/default_gmmanager_runprojects.php
+++ views/projects/tmpl/default_gmmanager_runprojects.php
@@ -223,6 +223,7 @@ $model = Gm_ceilingHelpersGm_ceiling::getModel('calculations');
                 mouting_sum = jQuery(this).closest("tr").find("#"+td.data("project_id")+"_mounting_sum").val(),
                 material_sum = jQuery(this).closest("tr").find("#"+td.data("project_id")+"_material_sum").val();
 				var subject = "Отметка стоимости договора №" + td.data("project_id");
+				var text;
 				if (status == "17") {
                     text = "<p><input name='check_mount' onclick='changeDone(this," + new_value + "," + mouting_sum + "," + material_sum + ");' class='radio' id ='done' value='1'  type='radio' checked><label for = 'done'>Монтаж выполнен</label></p>";
                     text += "<p><input name='check_mount' onclick='changeDone(this," + new_value + "," + mouting_sum + "," + material_sum + ");'  class='radio' id ='not_done' value='0'  type='radio'><label for = 'not_done'>Монтаж недовыполнен</label></p>";
