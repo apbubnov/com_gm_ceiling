c1a8ae7b8d4b91b86a57c56d721e6c911775ebc9
DenchikPushka <void.anonim@gmail.com>
2018-01-15 16:29:57 +0300

    redir4

--- views/clientcard/tmpl/default_dealer.php
+++ views/clientcard/tmpl/default_dealer.php
@@ -22,7 +22,7 @@
     $dealer = JFactory::getUser($client->dealer_id);
     if ($dealer->associated_client != $this->item->id)
     {
-        header("location:/index.php?option=com_gm_ceiling&view=clientcard&id=$this->item->id");
+        header("Location:/index.php?option=com_gm_ceiling&view=clientcard&id=$this->item->id");
         exit;
     }
     
