8d939a9626b64a05138df25ba3acb9c431192adb
DenchikPushka <void.anonim@gmail.com>
2018-01-15 11:49:10 +0300

    ex

--- views/projects/tmpl/default_gmmanager.php
+++ views/projects/tmpl/default_gmmanager.php
@@ -13,6 +13,7 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('formbehavior.chosen', 'select');
+throw new Exception("Error Processing Request", 1);
 
 $user       = JFactory::getUser();
 $userId     = $user->get('id');
