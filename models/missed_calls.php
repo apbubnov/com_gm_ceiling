6a9b4a7773f0da671322d3e7eeb329f200471aca
DenchikPushka <void.anonim@gmail.com>
2018-01-16 11:34:37 +0300

    missed calls 7

--- models/missed_calls.php
+++ models/missed_calls.php
@@ -31,5 +31,27 @@ class Gm_ceilingModelMissed_Calls extends JModelList
             throw new Exception('Ошибка!', 500);
         }
 	}
+
+	function addCall($call_id, $manager_id)
+	{
+		try
+		{
+			$db = JFactory::getDbo();
+			$query = $db->getQuery(true);
+			$query->insert('`#__gm_ceiling_missed_calls`');
+			$query->columns('`call_id`,`manager_id`');
+			$query->values("'$call_id', $manager_id");
+			$db->setQuery($query);
+			$db->execute();
+			return true;
+		}
+		catch(Exception $e)
+        {
+            $date = date("d.m.Y H:i:s");
+            $files = "components/com_gm_ceiling/";
+            file_put_contents($files.'error_log.txt', (string)$date.' | '.__FILE__.' | '.__FUNCTION__.' | '.$e->getMessage()."\n----------\n", FILE_APPEND);
+            throw new Exception('Ошибка!', 500);
+        }
+	}
 }
 ?>
