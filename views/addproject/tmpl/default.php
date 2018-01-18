40d1615a5eac973949fd8b9e7526a264ea782465
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-18 14:46:25 +0300

    тоже 9

--- views/addproject/tmpl/default.php
+++ views/addproject/tmpl/default.php
@@ -318,7 +318,7 @@ if (count($AllGaugerDealer) == 0) {
 			height1 = jQuery("#div1").css("height");
 			height2 = jQuery("#div2").css("height");
 			height = heightAll - height1 - height2;
-			jQuery("#table_wraper").css("height", height);
+			jQuery("#table_wraper").css({"height", height; "overflow-y": "auto"});
 			if (jQuery("#jform_who_calculate1").attr("checked") == "checked") {
 				var dealer = 1;
 			} else {
