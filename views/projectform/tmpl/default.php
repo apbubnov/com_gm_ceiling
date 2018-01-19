7bd61ca5b6f9c89d1c29575338b952790af9b8cc
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-19 10:39:14 +0300

    попытка 3

--- views/projectform/tmpl/default.php
+++ views/projectform/tmpl/default.php
@@ -1010,6 +1010,7 @@ echo parent::getPreloader();
                                 </tr>
                             </table>
                         </div>
+                    <?php } ?>
                     <?php if ($userId != $user->dealer_id) { ?>
                         <input name="type" value="chief" type="hidden">
                     <?php } else { ?>
