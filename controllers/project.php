78c01b6fbb912a3b146e0f1668b740be9c2d0936
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-19 09:39:39 +0300

    редирект после сохранения времени на монтаж

--- controllers/project.php
+++ controllers/project.php
@@ -1076,7 +1076,7 @@ class Gm_ceilingControllerProject extends JControllerLegacy
 			}
 			if($type === "gmchief") {
 				$this->setRedirect(JRoute::_('index.php?option=com_gm_ceiling&view=projects&type=gmchief', false));
-			} elseif($type === "chiefprojects") {
+			} elseif($type === "chief" && $user->dealer_type == 1 && $old_date == "0000-00-00 00:00:00") {
 				$this->setRedirect(JRoute::_('index.php?option=com_gm_ceiling&view=projects&type=chiefprojects', false));
 			} else {
 				$this->setRedirect(JRoute::_('index.php?option=com_gm_ceiling&view=projects&type=chief', false));
