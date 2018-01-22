47274899564c8b9f4c3af419068d9c2915dcf1c9
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-22 12:42:34 +0300

    замерщики 2

--- models/projects.php
+++ models/projects.php
@@ -273,17 +273,9 @@ class Gm_ceilingModelProjects extends JModelList
 
             // замерщик (график замеров), НМС (замеры), дилер (замерщик)
             if ($status == "GaugingsGraph") {
-                $query2 = $db->getQuery(true);
-                $query2->select('group_id')
-                    ->from('#__users as users')
-                    ->innerJoin("#__user_usergroup_map as map ON users.id = map.user_id")
-                    ->where("users.id = '$userId'");
-                $db->setQuery($query2);
-                $group = $db->loadObject();
-                echo $group->group_id;
-
-                if ($group->group_id == 12 || $group->group_id == 14 || $group->group_id == 17) {
-                    if ($user->dealer_id == 1) {
+                $groups = $user->groups;
+                if (in_array("12", $groups) || in_array("14", $groups) || in_array("17", $groups)) {
+                    if ($user->groups == 1) {
                         $who = "1";
                     } else {
                         $who = "0";
@@ -292,7 +284,7 @@ class Gm_ceilingModelProjects extends JModelList
                         ->from('#__gm_ceiling_projects as projects')
                         ->innerJoin("#__gm_ceiling_clients as clients ON projects.client_id = clients.id")
                         ->where("projects.project_status = '1' and projects.who_calculate = '$who' and clients.dealer_id = '$user->dealer_id'");    
-                } else if ($group->group_id == 21 || $group->group_id == 2) {
+                } else if (in_array("21", $groups) || in_array("22", $groups)) {
                     $query->select('count(projects.id) as count')
                         ->from('#__gm_ceiling_projects as projects')
                         ->where("projects.project_status = '1' and projects.project_calculator  = '$userId'");
