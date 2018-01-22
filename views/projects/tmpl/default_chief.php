f70ab4dcc4e09a60f522b9d18bc982c64947c6df
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-22 12:10:22 +0300

    вывод кнопки выполнено у монтажника

--- views/projects/tmpl/default_chief.php
+++ views/projects/tmpl/default_chief.php
@@ -89,15 +89,11 @@ $canDelete = $user->authorise('core.delete', 'com_gm_ceiling');
                         <tr data-href="<?= JRoute::_('index.php?option=com_gm_ceiling&view=projectform&type=chief&id=' . (int)$item->id); ?>">
 
                             <td>
-                                <? if ($item->project_status == 10): ?>
-<!--                                --><?// //elseif ($item->project_status == 8): ?><!-- Требует утверждения-->
-<!--                                --><?// //elseif ($item->project_status == 10): ?>
-                                    <? //if ($user->dealer_id == $item->brigadir_id): ?>
+                                <? if ($item->project_status == 10 || ($user->dealer_type == 1 && $item->project_status == 5)):  ?>
                                         <button class="btn btn-primary btn-done" data-project_id="<?= $item->id; ?>"
                                                 type="button">Выполнено
                                         </button>
-<!--                                    --><?// //else: ?><!--Запущен. Монтаж ГМ--><? //endif; ?>
-<!--                                --><?// //else: ?><!--Требует утверждения--><? endif; ?>
+                                        
                             </td>
                             <td class="center one-touch">
                                 <?= $item->id; ?>
