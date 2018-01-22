dc1a49a054039fa4b8d41309ca04afcfff0856f2
apbubnov <al.p.bubnov@gmail.com>
2018-01-22 14:25:30 +0300

    fix who_calculate

--- controllers/project.php
+++ controllers/project.php
@@ -120,9 +120,6 @@ class Gm_ceilingControllerProject extends JControllerLegacy
 	public function recToMeasurement()
 	{
 		try {
-			$date = date("d.m.Y H:i:s");
-            $files = "components/com_gm_ceiling/";
-            file_put_contents($files.'error_log.txt', (string)$date."wtf?wtf?wtf?wtf?wtf?wtf?wtf?wtf?\n", FILE_APPEND);
             $app = JFactory::getApplication();
 			$user = JFactory::getUser();
 			$user_group = $user->groups;
