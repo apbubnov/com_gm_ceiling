b439214a53e3d85730a5866279c68378394bdf11
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-23 16:40:31 +0300

    вкладка общее

--- views/project/tmpl/default_calculator_calendar.php
+++ views/project/tmpl/default_calculator_calendar.php
@@ -539,15 +539,16 @@ $results = $db->loadObjectList();
     <li class="nav-item">
         <a class="nav-link <?php if($user->dealer_type == 0 || count($calculations) == 0) echo "active";?>" data-toggle="tab" href="#summary" role="tab">Общее</a>
     </li>
-    
+
     <?php foreach ($calculations as $k => $calculation) { ?>
         <li class="nav-item">
         <?php if($user->dealer_type == 1) { ?>
             <a class="nav-link active" data-toggle="tab" href="#calculation<?php echo $calculation->id; ?>"
                 role="tab"><?php echo $calculation->calculation_title; ?></a>
-         <?}?>
+         <?} else { ?>
          <a class="nav-link" data-toggle="tab" href="#calculation<?php echo $calculation->id; ?>"
                 role="tab"><?php echo $calculation->calculation_title; ?></a>
+         <?}?>
         </li>
     <?php } ?>
     <li class="nav-item"> 
