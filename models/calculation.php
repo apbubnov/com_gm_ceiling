8c63a69a4e5f237f0c726d20d1c12f804bd5fe63
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-23 11:54:06 +0300

    получение запроса

--- models/calculation.php
+++ models/calculation.php
@@ -244,6 +244,7 @@ class Gm_ceilingModelCalculation extends JModelItem
 					->from('`#__gm_ceiling_clients` as client')
 					->join('LEFT','`#__gm_ceiling_calculations` AS c ON c.client_id = client.id')
 					->where($db->quoteName('с.id') . ' = ' . $db->quote($this->_item->id));
+					print_r((string)$query); exit;
 				$db->setQuery($query);
 				$this->_item->dealer_id = $db->loadObject()->dealer_id;
 				
