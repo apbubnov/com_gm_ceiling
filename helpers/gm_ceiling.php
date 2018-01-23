f51e6f9be1730a984833ee9a8d0e2f4cf14e4eb9
apbubnov <al.p.bubnov@gmail.com>
2018-01-23 16:00:46 +0300

    fix bugs in calc_transport #39

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -3787,7 +3787,7 @@ class Gm_ceilingHelpersGm_ceiling
             $data = array(
                 'id'=> $project_id,
                 'transport'=>$transport_type,
-                '$distance' => $distance,
+                'distance' => $distance,
                 'distance_col' =>$distance_col
             );
             $res = $project_model->transport((object)$data);
@@ -3812,7 +3812,7 @@ class Gm_ceilingHelpersGm_ceiling
                     'distance' => '-',
                     'distance_col'=> $distance_col,
                     'client_sum' => $transport_sum,
-                    'mounter_sum' => $transport_sum1 
+                    'mounter_sum' => $transport_sum_1 
                 );
 
             }
@@ -3826,10 +3826,10 @@ class Gm_ceilingHelpersGm_ceiling
                   }
                 $result = array(
                     'transport' => 'Выезд за город',
-                    'distance' => '-',
+                    'distance' => $distance,
                     'distance_col'=> $distance_col,
                     'client_sum' => $transport_sum,
-                    'mounter_sum' => $transport_sum1 
+                    'mounter_sum' => $transport_sum_1 
                 );  
             }
             else { 
@@ -3840,7 +3840,7 @@ class Gm_ceilingHelpersGm_ceiling
                     'distance' => '-',
                     'distance_col'=> '-',
                     'client_sum' => $transport_sum,
-                    'mounter_sum' => $transport_sum1 
+                    'mounter_sum' => $transport_sum_1 
                 );
             } 
         }
