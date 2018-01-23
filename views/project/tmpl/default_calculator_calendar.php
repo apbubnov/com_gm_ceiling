0b186b9dbbfba69b51077f2b38bcca02945d29dd
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-23 14:52:36 +0300

    попытка 3

--- views/project/tmpl/default_calculator_calendar.php
+++ views/project/tmpl/default_calculator_calendar.php
@@ -555,7 +555,7 @@ $results = $db->loadObjectList();
 
 <!-- Tab panes -->
 <div class="tab-content">
-<?php if($user->dealer_type == 1 && count($calculations) == 0):?>
+<?php if($user->dealer_type == 1 && count($calculations) != 0):?>
     <div class="tab-pane active" id="summary" role="tabpanel">
         <table id="table1" class="table table-striped one-touch-view">
             <tr>
