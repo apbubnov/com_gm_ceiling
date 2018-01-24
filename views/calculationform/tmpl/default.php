f0fcf0e8ed49afc0dae1ae10f08549a133627cba
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-24 09:49:10 +0300

    просчетная 3

--- views/calculationform/tmpl/default.php
+++ views/calculationform/tmpl/default.php
@@ -331,15 +331,17 @@
 					</div>
 				</div>
 				<div class="col-sm-4" style="padding-right: 0px;">
-					<div class="form-group">
-						<? if ($this->item->n28 == 0) {
-							?>
-							<p><input name="jform[n28]" id="jform_n28" class="radio" value="0" type="radio"
-										checked="checked"><label for="jform_n28"> Обычный багет</label></p>
-							<p><input name="jform[n28]" id="jform_n28_1" class="radio" value="1"
-										type="radio"><label for="jform_n28_1"> Потолочный багет</label></p>
-							<p><input name="jform[n28]" id="jform_n28_2" class="radio" value="2"
-										type="radio"><label for="jform_n28_2"> Алюминиевый багет</label></p>
+					<div class="form-group" style="text-align: left; /*margin-left: calc(50% - px);*/">
+						<? if ($this->item->n28 == 0) { ?>
+							<p style="width: auto;">
+								<input name="jform[n28]" id="jform_n28" class="radio" value="0" type="radio" checked="checked"><label for="jform_n28"> Обычный багет</label>
+							</p>
+							<p>
+								<input name="jform[n28]" id="jform_n28_1" class="radio" value="1" type="radio"><label for="jform_n28_1"> Потолочный багет</label>
+							</p>
+							<p>
+								<input name="jform[n28]" id="jform_n28_2" class="radio" value="2" type="radio"><label for="jform_n28_2"> Алюминиевый багет</label>
+							</p>
 						<? } elseif ($this->item->n28 == 1) { ?>
 							<p><input name="jform[n28]" id="jform_n28" class="radio" value="0" type="radio">
 								<label for="jform_n28"> Обычный багет</label></p>
