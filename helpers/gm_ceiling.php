8ce8d8302ac9745233edf5e9eaa081ddb9e0d954
apbubnov <al.p.bubnov@gmail.com>
2018-01-24 14:58:42 +0300

    change #39

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -2795,7 +2795,7 @@ class Gm_ceilingHelpersGm_ceiling
         $n26 = json_decode($data['n26']);
         $n22 = json_decode($data['n22']);
         $n14 = json_decode($data['n14']);
-        throw new Exception(count($n14));
+        throw new Exception(count($data['n14']));
         $n23 = json_decode($data['n23']);
         $n15 = json_decode($data['n15']);
         $n29 = json_decode($data['n29']);
