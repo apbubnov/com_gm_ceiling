a291ad9d2e014c520a1048cbaff2756102c48807
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-24 12:21:28 +0300

    tozeh 3 #34

--- views/projectform/tmpl/default.php
+++ views/projectform/tmpl/default.php
@@ -1513,9 +1513,9 @@
                         // вывод времени бригады
                         var BusyTimes = [];
                         Array.from(data).forEach(function(elem) {
-                            if (selectedBrigade == elem.project_mounter and elem.project_mounting_day_off == "") {
+                            if (selectedBrigade == elem.project_mounter /* and elem.project_mounting_day_off == "" */) {
                                 BusyTimes.push(elem.project_mounting_date.substr(11));
-                            } else if (selectedBrigade == elem.project_mounter and elem.project_mounting_day_off != "") {
+                            } else if (selectedBrigade == elem.project_mounter /* and elem.project_mounting_day_off != "" */) {
                                 AllTime.forEach(element => {
                                     if (element >= elem.project_mounting_date.substr(11) and element <= elem.project_mounting_day_off.substr(11)) {
                                         BusyTimes.push(element);
