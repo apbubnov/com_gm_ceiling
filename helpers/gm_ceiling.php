a9681fbb7102f63f648b9232e4fbee6b06302bae
apbubnov <al.p.bubnov@gmail.com>
2018-01-25 16:41:16 +0300

    check components #39

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -2771,7 +2771,7 @@ class Gm_ceilingHelpersGm_ceiling
             return $return;
     }
     public static function calculate_components($calc_id,$data,$del_flag){
-        $calculations_model = self::getModel('calculations');
+        $calculation_model = self::getModel('calculation');
         $calculation_data = $calculation_model->getData($calc_id);
         if(!empty($calc_id)){
             foreach ($calculation_data as $key => $item) {
