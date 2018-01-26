90bcd3d8769f17adb0d714e74b3cb4bd9aa745d8
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-26 15:34:25 +0300

    #75 ексепшн

--- views/mountersorder/tmpl/default.php
+++ views/mountersorder/tmpl/default.php
@@ -304,7 +304,8 @@ if (!empty($calculation_ids)) {
                                             if (!in_array($val["title"], $rest)) { 
                                                 $rest[$val["title"]] = ["title"=>$val["title"], "gm_salary"=>$val["gm_salary"], "dealer_salary"=>$val["dealer_salary"], "quantity"=>$val["quantity"], "gm_salary_total"=>$val["gm_salary_total"], "dealer_salary_total"=>$val["dealer_salary_total"]];
                                             } else {
-                                                var_dump($val["title"]);
+                                                throw new Exception($val["title"]);
+                                                
                                                 $rest[$val["title"]]["gm_salary"] += $val["gm_salary"];
                                                 $rest[$val["title"]]["dealer_salary"] += $val["dealer_salary"];
                                                 $rest[$val["title"]]["quantity"] += $val["quantity"];
