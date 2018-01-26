dacab989a7a63a4f779fb4f0c4df05ef501bffe0
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-26 15:33:07 +0300

    #75 g

--- views/mountersorder/tmpl/default.php
+++ views/mountersorder/tmpl/default.php
@@ -304,14 +304,12 @@ if (!empty($calculation_ids)) {
                                             if (!in_array($val["title"], $rest)) { 
                                                 $rest[$val["title"]] = ["title"=>$val["title"], "gm_salary"=>$val["gm_salary"], "dealer_salary"=>$val["dealer_salary"], "quantity"=>$val["quantity"], "gm_salary_total"=>$val["gm_salary_total"], "dealer_salary_total"=>$val["dealer_salary_total"]];
                                             } else {
-                                                echo $val["title"]."\n";
-                                                echo $rest[$val["title"]]["quantity"]."\n";
+                                                var_dump($val["title"]);
                                                 $rest[$val["title"]]["gm_salary"] += $val["gm_salary"];
                                                 $rest[$val["title"]]["dealer_salary"] += $val["dealer_salary"];
                                                 $rest[$val["title"]]["quantity"] += $val["quantity"];
                                                 $rest[$val["title"]]["gm_salary_total"] += $val["gm_salary_total"];
                                                 $rest[$val["title"]]["dealer_salary_total"] += $val["dealer_salary_total"];
-                                                echo $rest[$val["title"]]["quantity"]."\n";
                                             }
                                         }
                                     ?>
