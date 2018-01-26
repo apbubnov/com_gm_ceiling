c1046f6697b00a2a7d49df26d7560b9ea63beb93
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-26 15:26:54 +0300

    #75 п\опытка сделать общее раз сотый

--- views/mountersorder/tmpl/default.php
+++ views/mountersorder/tmpl/default.php
@@ -187,13 +187,13 @@ if (!empty($calculation_ids)) {
                                             }
                                         } else {
                                             if (!in_array($val["title"], $rest)) { 
-                                                array_push($rest, ["title"=>$val["title"], "gm_salary"=>$val["gm_salary"], "dealer_salary"=>$val["dealer_salary"], "quantity"=>$val["quantity"], "gm_salary_total"=>$val["gm_salary_total"], "dealer_salary_total"=>$val["dealer_salary_total"]]);
+                                                $rest[$val["title"]] = ["title"=>$val["title"], "gm_salary"=>$val["gm_salary"], "dealer_salary"=>$val["dealer_salary"], "quantity"=>$val["quantity"], "gm_salary_total"=>$val["gm_salary_total"], "dealer_salary_total"=>$val["dealer_salary_total"]];
                                             } else {
-                                                $rest["gm_salary"] += $val["gm_salary"];
-                                                $rest["dealer_salary"] += $val["dealer_salary"];
-                                                $rest["quantity"] += $val["quantity"];
-                                                $rest["gm_salary_total"] += $val["gm_salary_total"];
-                                                $rest["dealer_salary_total"] += $val["dealer_salary_total"];
+                                                $rest[$val["title"]]["gm_salary"] += $val["gm_salary"];
+                                                $rest[$val["title"]]["dealer_salary"] += $val["dealer_salary"];
+                                                $rest[$val["title"]]["quantity"] += $val["quantity"];
+                                                $rest[$val["title"]]["gm_salary_total"] += $val["gm_salary_total"];
+                                                $rest[$val["title"]]["dealer_salary_total"] += $val["dealer_salary_total"];
                                             }
                                         }
                                     ?>
@@ -301,16 +301,14 @@ if (!empty($calculation_ids)) {
                                                 $cloth["dealer_salary_total"] += $val["dealer_salary_total"];
                                             }
                                         } else {
-                                            var_dump($rest);
-                                            echo $val["title"];
                                             if (!in_array($val["title"], $rest)) { 
-                                                array_push($rest, ["title"=>$val["title"], "gm_salary"=>$val["gm_salary"], "dealer_salary"=>$val["dealer_salary"], "quantity"=>$val["quantity"], "gm_salary_total"=>$val["gm_salary_total"], "dealer_salary_total"=>$val["dealer_salary_total"]]);
+                                                $rest[$val["title"]] = ["title"=>$val["title"], "gm_salary"=>$val["gm_salary"], "dealer_salary"=>$val["dealer_salary"], "quantity"=>$val["quantity"], "gm_salary_total"=>$val["gm_salary_total"], "dealer_salary_total"=>$val["dealer_salary_total"]];
                                             } else {
-                                                $rest["gm_salary"] += $val["gm_salary"];
-                                                $rest["dealer_salary"] += $val["dealer_salary"];
-                                                $rest["quantity"] += $val["quantity"];
-                                                $rest["gm_salary_total"] += $val["gm_salary_total"];
-                                                $rest["dealer_salary_total"] += $val["dealer_salary_total"];
+                                                $rest[$val["title"]]["gm_salary"] += $val["gm_salary"];
+                                                $rest[$val["title"]]["dealer_salary"] += $val["dealer_salary"];
+                                                $rest[$val["title"]]["quantity"] += $val["quantity"];
+                                                $rest[$val["title"]]["gm_salary_total"] += $val["gm_salary_total"];
+                                                $rest[$val["title"]]["dealer_salary_total"] += $val["dealer_salary_total"];
                                             }
                                         }
                                     ?>
