2abd970073047cec1bee6277f6539ee9bc026356
apbubnov <al.p.bubnov@gmail.com>
2018-01-29 15:14:48 +0300

    #37

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -2815,19 +2815,14 @@ class Gm_ceilingHelpersGm_ceiling
         $new_total_discount = round($new_total * (1 - ($data['discount'] / 100)), 2);
         $html = '<h1>Смета по материалам и комплектующим</h1>';
         $html .= "<h1>Название: " . $data['calculation_title'] . "</h1>";
-        $html .= '<div width = 100%>';
-        $html .= '<div width = 33% style = "display: inline-block;" >';
+        $html .= '<columns column-count="3" vAlign="J"/>'
         $html .= '<p><b>Дилер: </b>' . $dealer->name . '</p>';
         $html .= '<p><h2>Дата: ' . date("d.m.Y") . '</h2></p>';
         $html .= '<p><h2>Общее: ' . $new_total . ' руб.</h2></p>';
-        $thml .= '</div>';
-        $html .= '<div width = 33% style = "display: inline-block;" >';
-        $html .= '<newcolumn /> <img src="' . $_SERVER['DOCUMENT_ROOT'] . "/calculation_images/" . md5("calculation_sketch" . $data['id']) . '.png" align="right" width="200" height="200"/></td>';    
-        $thml .= '</div>';  
-        $html .= '<div width = 33% style = "display: inline-block;" >';
-        $html .= '<newcolumn />'.$data['calc_data'];    
-        $thml .= '</div>';
-        $html .= '</div>';
+        $html .= '<columnbreak />';
+        $html .= '<img src="' . $_SERVER['DOCUMENT_ROOT'] . "/calculation_images/" . md5("calculation_sketch" . $data['id']) . '.png" align="right" width="200" height="200"/></td>'; 
+        $html .= '<columnbreak />';   
+        $html .= $data['calc_data'];    
         if ($data['discount'] != 0){
             $html .= '<h2>Общее (со скидкой): <strong>' . $new_total_discount . ' руб.</strong></h2>';
         }
