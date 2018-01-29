fa72b216ffc556c216afbdf95993cebc24a14230
apbubnov <al.p.bubnov@gmail.com>
2018-01-29 13:17:58 +0300

    #39

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -2789,7 +2789,8 @@ class Gm_ceilingHelpersGm_ceiling
         $offcut_square_data = self::calculate_offcut($calc_id);
         $components_data = self::calculate_components($calc_id,null,0);
         $guild_data = self::calculate_guild_jobs($calc_id);
-        $mounting_data = self::calculate_mount(0,$calc_id,null);
+        $result = self::calculate_mount(0,$calc_id,null);
+        $mounting_data = $result->mounting_data;
         if(!empty($calc_id)){
             $calculation_model = self::getModel('calculation');
             $data = get_object_vars($calculation_model->getData($calc_id));
