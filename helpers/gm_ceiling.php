15623ec1382262eddfd17d262d4d167cbabadb06
apbubnov <al.p.bubnov@gmail.com>
2018-01-29 10:34:57 +0300

    #37 1st attempt

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -2806,13 +2806,28 @@ class Gm_ceilingHelpersGm_ceiling
         $new_total = round($canvases_data['dealer_total'] + $offcut_square_data['dealer_total'] + $dealer_components_sum + $total_with_gm_dealer_margin + $total_with_gm_dealer_margin_guild, 2);
         $new_total_discount = round($new_total * (1 - ($data['discount'] / 100)), 2);
         $html = '<h1>Смета по материалам и комплектующим</h1>';
+    
         $html .= "<h1>Название: " . $data['calculation_title'] . "</h1>";
-        $html .= '<img src="' . $_SERVER['DOCUMENT_ROOT'] . "/calculation_images/" . md5("calculation_sketch" . $data['id']) . '.png"/>;';
+        $html .= '<table>';
+        $html .= '<tr>';
+        $html .= '<td rowspan = 4><img src="' . $_SERVER['DOCUMENT_ROOT'] . "/calculation_images/" . md5("calculation_sketch" . $data['id']) . '.png"/></td>'
+        $html .= '</tr>';
+        $html .= '<tr>';
+        $html .= '<td>';
         $html .= "<b>Дилер: </b>" . $dealer->name . "<br>";
-        $html .= '<h2>Дата: ' . date("d.m.Y") . '</h2>
-			      <h2>Общее: ' . $new_total . ' руб.</h2>';
-        if ($data['discount'] != 0)
-        $html .= '<h2>Общее (со скидкой): <strong>' . $new_total_discount . ' руб.</strong></h2>';
+        $html .= '</td>';
+        $html .= '</tr>';
+        $html .= '<tr>'; 
+        $html .= '<td><h2>Дата: ' . date("d.m.Y") . '</h2></td>';
+        $html .= '</tr>';
+        $html .= '<tr>'; 
+        $html .= '<td><h2>Общее: ' . $new_total . ' руб.</h2></td>';
+        $html .= '</tr>';
+        if ($data['discount'] != 0){
+            $html .= '<tr>'; 
+            $html .= '<td><h2>Общее (со скидкой): <strong>' . $new_total_discount . ' руб.</strong></h2></td>';
+            $html .= '</tr>';
+        }
         $html .= '<table border="0" cellspacing="0" width="100%">
                     <tbody>
                         <tr>
