a7fa877465858c9610fec325b901c8394b8057f1
apbubnov <al.p.bubnov@gmail.com>
2018-01-30 09:02:01 +0300

    #37

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -2819,12 +2819,12 @@ class Gm_ceilingHelpersGm_ceiling
         $new_total_discount = round($new_total * (1 - ($data['discount'] / 100)), 2);
         $html = '<h1>Смета по материалам и комплектующим</h1>';
         $html .= "<h1>Название: " . $data['calculation_title'] . "</h1>";
-        $html .= '<table style = "border:0">';
+        $html .= '<table class = "no_border">';
         $html .= '<tr>';
-        $html .= '<td style = "border:0" >' . $dealer->name . '</td><td rowspan = "3"><img src="' . $_SERVER['DOCUMENT_ROOT'] . "/calculation_images/" . md5("calculation_sketch" . $data['id']) . '.png" align="right" width="200" height="200"/></td><td rowspan = "3">'.str_replace(';','; ',$data['calc_data']).'</td>';
+        $html .= '<td>' . $dealer->name . '</td><td rowspan = "3"><img src="' . $_SERVER['DOCUMENT_ROOT'] . "/calculation_images/" . md5("calculation_sketch" . $data['id']) . '.png" align="right" width="200" height="200"/></td><td rowspan = "3">'.str_replace(';','; ',$data['calc_data']).'</td>';
         $html .= '</tr>';
         $html .= '<tr>';
-        $html .= '<td style = "border:0"><h2>Дата: ' . date("d.m.Y") . '</h2></td>';
+        $html .= '<td><h2>Дата: ' . date("d.m.Y") . '</h2></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<td><h2>Общее: ' . $new_total . ' руб.</h2></td>';
