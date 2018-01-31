210a643e5bd4a978f04abebec292c47b58c5743f
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-31 12:44:26 +0300

    #118

--- views/clientcard/tmpl/default.php
+++ views/clientcard/tmpl/default.php
@@ -168,7 +168,7 @@
 
             <tr class = "row_project" data-href="<?php if($user->dealer_type == 1) {
                 if($item->status == "Отказ от договора" || $item->status == "Ждет замера" || $item->status == "Договор" ) echo JRoute::_('index.php?option=com_gm_ceiling&view=project&type=calculator&subtype=calendar&id='.(int) $item->id);
-                elseif($item->status == "В производстве" || $item->status == "Ожидание монтажа" || "Заказ закрыт") echo JRoute::_('index.php?option=com_gm_ceiling&view=project&type=calculator&subtype=calendar&id='.(int) $item->id);
+                elseif($item->status == "В производстве" || $item->status == "Ожидание монтажа" || $item->status == "Заказ закрыт") echo JRoute::_('index.php?option=com_gm_ceiling&view=project&type=calculator&subtype=calendar&id='.(int) $item->id);
                 else echo JRoute::_('index.php?option=com_gm_ceiling&view=project&type=manager&subtype=calendar&id='.(int) $item->id); }
                 else {  echo JRoute::_('index.php?option=com_gm_ceiling&view=project&type=gmmanager&subtype='.$subtype.'&id='.(int) $item->id.'&call_id='.(int) $call_id); }?>">
                 <td><?php echo $item->id;?></td>
