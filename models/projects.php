3c82a0f75798dbc58b58e7bef9bfa91ddbebc1b8
MISS-CEH4TOP <popowa.alinochka@gmail.com>
2018-01-31 10:24:41 +0300

    #118 пункт 1

--- models/projects.php
+++ models/projects.php
@@ -839,4 +839,25 @@ class Gm_ceilingModelProjects extends JModelList
             throw new Exception('Ошибка!', 500);
         }
     }
+
+    public function deleteEmptyProject($id)
+    {
+        try
+        {
+            $db = $this->getDbo();
+            $query = $db->getQuery(true);
+            $query->delete($db->quoteName('#__gm_ceiling_projects'));
+            $query->where('project_status = 0 AND client_id = ' . $id);
+            $db->setQuery($query);
+            $db->execute();
+            return 1;
+        }
+        catch(Exception $e)
+        {
+            $date = date("d.m.Y H:i:s");
+            $files = "components/com_gm_ceiling/";
+            file_put_contents($files.'error_log.txt', (string)$date.' | '.__FILE__.' | '.__FUNCTION__.' | '.$e->getMessage()."\n----------\n", FILE_APPEND);
+            throw new Exception('Ошибка!', 500);
+        }
+    }
 }
