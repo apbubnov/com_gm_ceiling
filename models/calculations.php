d4eb72bc768368c66dbc09316836113c8ff182a3
Nataliya1996 <natycik-mycik@mail.ru>
2018-01-31 11:58:53 +0300

    тоже

--- models/calculations.php
+++ models/calculations.php
@@ -1090,6 +1090,7 @@ class Gm_ceilingModelCalculations extends JModelList {
                     array_push($items, $day);
                 }
             }
+throw new Exception(count($items));
 
     		return $items;
         }
