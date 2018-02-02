020c61dbe68387ca1cb1c9aebb09563e3618177a
apbubnov <al.p.bubnov@gmail.com>
2018-02-02 13:26:03 +0300

    #39

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -546,7 +546,7 @@ class Gm_ceilingHelpersGm_ceiling
                
                 //наряд монтажной бригаде
                 if($need_mount){
-                    //self::create_single_mount_estimate(null,$data,$mounting_data);
+                    self::create_single_mount_estimate(null,$data,$mounting_data);
                 }       
                 //PDF раскроя
                 //self::create_cut_pdf(null,$data);
@@ -2768,7 +2768,8 @@ class Gm_ceilingHelpersGm_ceiling
             
             $html = self::create_single_mounter_estimate_html($calc_id,$data,$phones,$brigade,$brigade_names,$data_mount);
 
-            $filename = md5($data['id'] . "mount_single") . ".pdf";
+            
+            $filename = md5( . "mount_single") . ".pdf";
            
             
             $sheets_dir = $_SERVER['DOCUMENT_ROOT'] . '/costsheets/';
