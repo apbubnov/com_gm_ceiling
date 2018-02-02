a4cd5a64c232c5223ce8ec43b523d45ce3506ecf
apbubnov <al.p.bubnov@gmail.com>
2018-02-02 13:30:37 +0300

    #39

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -2769,7 +2769,7 @@ class Gm_ceilingHelpersGm_ceiling
             $html = self::create_single_mounter_estimate_html($calc_id,$data,$phones,$brigade,$brigade_names,$data_mount);
 
             
-            $filename = md5( . "mount_single") . ".pdf";
+            $filename = md5( "mount_single") . ".pdf";
            
             
             $sheets_dir = $_SERVER['DOCUMENT_ROOT'] . '/costsheets/';
