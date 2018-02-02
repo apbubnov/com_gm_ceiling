176674774c4b9fe35741f0f6d30ab551cd16d909
Vasilev Dmitry <ceh4top@gmail.com>
2018-02-02 11:57:46 +0300

    TEst

--- models/projects.php
+++ models/projects.php
@@ -886,8 +886,7 @@ class Gm_ceilingModelProjects extends JModelList
                 ->select("p.project_info as address")
                 ->select("client.client_name as client_name, client.created as created, client.id as client_id")
                 ->select("GROUP_CONCAT(phone.phone SEPARATOR ', ') as client_contacts")
-                ->group("client.client_name")
-                ->group("p.id");
+                ->group("client.id");
 
             if($status && !$search)
                 $query->where('p.project_status = '. $status . ' and client.dealer_id = '. $user->dealer_id);
