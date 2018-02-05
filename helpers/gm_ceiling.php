7063e0fa37267a2ffd8b690cebd356b57fb133e1
Vasilev Dmitry <ceh4top@gmail.com>
2018-02-05 10:38:25 +0300

    SVG Реализация // Исправление багов // #115

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -143,18 +143,12 @@ class Gm_ceilingHelpersGm_ceiling
         JFactory::getApplication()->enqueueMessage("Добавлен новый дилер!");
         $margin_model = self::getModel('Dealer_info');
         $mount_model = self::getModel('mount');
-        if ($type == 3)
-        {
+
             $gm_margin = $margin_model->getDataById(1);
             $margin_model->save($gm_margin->dealer_canvases_margin,$gm_margin->dealer_components_margin,$gm_margin->dealer_mounting_margin,$gm_margin->gm_canvases_margin,$gm_margin->gm_components_margin,$gm_margin->gm_mounting_margin,$userID,$gm_margin->discount);
             $gm_mount = $mount_model->getDataAll(1);
             $gm_mount->user_id = $userID;
             $mount_model->insert($gm_mount);
-        }
-        else
-        {
-            $margin_model->save(0,0,0,0,0,0,$userID,0);
-        }
         
         return $userID;
 
