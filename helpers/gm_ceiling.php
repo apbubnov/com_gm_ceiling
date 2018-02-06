c2a0e0cc9c42ae23f0ed9b1449e33bf8287644ca
Vasilev Dmitry <ceh4top@gmail.com>
2018-02-06 11:24:23 +0300

    Просчеты

--- helpers/gm_ceiling.php
+++ helpers/gm_ceiling.php
@@ -506,11 +506,11 @@ class Gm_ceilingHelpersGm_ceiling
                 $k = []; $number = 1;
                 foreach ($calculations as $calculation) {
                     $calculation_name = $calculation->calculation_title;
-                    $index = intval(str_replace("Потолок", "", $calculation_name));
+                    $index = intval(str_replace("Потолок ", "", $calculation_name));
                     $k[] = $index;
                 }
                 while(in_array($number, $k)) $number++;
-                $data['calculation_title'] = "Потолок " . $k;
+                $data['calculation_title'] = "Потолок " . $number;
             }
             //Сохранение калькуляции
             $calculation_model = Gm_ceilingHelpersGm_ceiling::getModel('CalculationForm', 'Gm_ceilingModel');
