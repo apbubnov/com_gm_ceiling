c88ca977dcbdab4426766f331379bcf084f4d748
apbubnov <al.p.bubnov@gmail.com>
2018-02-14 10:43:34 +0300

    edit min_sum of project

--- views/dealerprofile/tmpl/default_edit.php
+++ views/dealerprofile/tmpl/default_edit.php
@@ -390,15 +390,15 @@ $mount = $model_mount->getDataAll();
 		</div>
 	</div>
 	<h3>
-		Минимальная сумма монтажа
+		Минимальная сумма заказа
 	</h3>
 	<div class="col-md-4">
 		<div class="control-group">
 			<div class="control-label">
-				<label id="jform_min_mount-lbl" for="jform_min_mount" >Монтаж</label>
+				<label id="jform_min_sum-lbl" for="jform_min_sum" >Минимальная сумма</label>
 			</div>
 			<div class="controls">
-				<input type="text" name="jform[min_mount]" id="jform_min_mount" value=<?php echo ($mount->min_mount)?$mount->min_mount:0 ?> class="required" style="width:100%;" size="3" required aria-required="true" />				
+				<input type="text" name="jform[min_sum]" id="jform_min_sum" value=<?php echo ($mount->min_sum)?$mount->min_sum:0 ?> class="required" style="width:100%;" size="3" required aria-required="true" />				
 			</div>
 		</div>
 	</div>
