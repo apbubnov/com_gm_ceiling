749e503f66a5648ffd394d9a5f5c33b99fe9ef3b
apbubnov <al.p.bubnov@gmail.com>
2018-02-22 10:32:44 +0300

    новый

--- views/dealers/tmpl/default.php
+++ views/dealers/tmpl/default.php
@@ -65,8 +65,8 @@ $recoil_map_model = Gm_ceilingHelpersGm_ceiling::getModel('recoil_map_project');
                 <tr class="row<?php echo $i % 2; ?>" data-href="<?php echo JRoute::_('index.php?option=com_gm_ceiling&view=clientcard&type=dealer&id='.(int) $value->associated_client); ?>">
 		            <td>
                        <?php echo $value->name;
-                        if($value->kp_cnt + $value->cmnt_cnt + $value->inst_cnt){
-                            echo '&#9734';
+                        if($value->kp_cnt + $value->cmnt_cnt + $value->inst_cnt == 0 ){
+                            echo "<label class = \"new_dealer\">Новый</label>";
                         }?>
 		            </td>
                     <td>
