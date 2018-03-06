ccebe995651767105355ec5c645ea570a47c6756
Nataliya1996 <natycik-mycik@mail.ru>
2018-03-06 14:24:13 +0300

    тоже

--- views/dealerprofile/tmpl/default_edit.php
+++ views/dealerprofile/tmpl/default_edit.php
@@ -28,10 +28,8 @@ $mount = $model_mount->getDataAll();
 	.caption1 {
 		text-align: center;
 		padding: 10px 0;
-		border-radius: 0;
-		border-color: #ffffff;
-		border-style: solid;
 		height: auto;
+		border: 0;
 	}
 </style>
 
