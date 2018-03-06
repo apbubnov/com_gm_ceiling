e5bbbbeb31c7470a609c137bf678eef8fda88a1d
Nataliya1996 <natycik-mycik@mail.ru>
2018-03-06 15:13:21 +0300

    тоже

--- views/dealerprofile/tmpl/default_score.php
+++ views/dealerprofile/tmpl/default_score.php
@@ -33,7 +33,7 @@ $rest = -($total_sum) - $contributed;
     .small_table {
         font-size: 1rem;
     }
-    @media screen and (min-width: 768px) {
+    @media screen and (min-width: 992px) {
         .small_table {
             font-size: 13px;
         }
