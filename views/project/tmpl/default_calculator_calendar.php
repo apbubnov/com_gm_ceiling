4e7f64fa532c10b921047bd439f800510e0e14eb
Nataliya1996 <natycik-mycik@mail.ru>
2018-03-12 15:43:11 +0300

    тоже

--- views/project/tmpl/default_calculator_calendar.php
+++ views/project/tmpl/default_calculator_calendar.php
@@ -594,7 +594,7 @@ $Transport->itog_sum = $mount_transport->distance * $this->item->distance * $thi
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-6 no_padding">
-                <h4>Информация по проекту № <?php echo $this->item->id ?></h4>
+                <h4 style="margin-bottom: 15px;">Информация по проекту № <?php echo $this->item->id ?></h4>
                 <form id="form-client" action="/index.php?option=com_gm_ceiling&task=project.activate&type=calculator&subtype=calendar" method="post" enctype="multipart/form-data">
                     <?php if ($this->type === "calculator" && $this->subtype === "calendar") { ?>
                         <?php if ($this->item->project_verdict == 0) { ?>
