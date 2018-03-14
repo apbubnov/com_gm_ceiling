64142893e0250527c70d8a4abe30f4583a033ccc
Nataliya1996 <natycik-mycik@mail.ru>
2018-03-14 16:11:51 +0300

    тоже

--- views/calculationform/tmpl/default.php
+++ views/calculationform/tmpl/default.php
@@ -308,7 +308,7 @@
 			<div class="row">
 				<div class="col-sm-4"></div>
 				<div class="col-sm-4">
-					<button type="button" id="btn_precalculation" class="btn add_fields btn-big" style="margin-bottom: 25px;">Добавить монтаж и комплектующие</button>
+					<button type="button" id="btn_precalculation" class="btn btn-big" style="margin-bottom: 25px;">Добавить монтаж и комплектующие</button>
 				</div>
 				<div class="col-sm-4"></div>
 			</div>
