7dfa923c05cb3a69c5b89aef5c7cbb9eba9e8ffd
Nataliya1996 <natycik-mycik@mail.ru>
2018-03-14 16:43:21 +0300

    тоже

--- views/calculationform/tmpl/default.php
+++ views/calculationform/tmpl/default.php
@@ -2067,7 +2067,6 @@
 
 		// кнопки открытия скрытых полей
 		precalculation = <?php if ($_GET['precalculation']) { echo $_GET['precalculation']; } else { echo 0; } ?>;
-		console.log(precalculation);
 		if (precalculation == 1) {
 			jQuery("#precalculation_container_hide").hide();
 			jQuery(".smeta_hide").hide();
