bf45eb09ed180031512d605254dc27bba043db88
Nataliya1996 <natycik-mycik@mail.ru>
2018-03-16 14:40:10 +0300

    кнопоньки просчетки

--- views/calculationform/tmpl/default.php
+++ views/calculationform/tmpl/default.php
@@ -2111,7 +2111,7 @@
 	jQuery(document).ready(function() {
 
 		// кнопки открытия скрытых полей
-		precalculation = <?php if ($_GET['precalculation']) { echo $_GET['precalculation']; } else { echo 0; } ?>;
+			precalculation = <?php if ($_GET['precalculation']) { echo $_GET['precalculation']; } else { echo 0; } ?>;
 			if (precalculation == 1) {
 				jQuery("#precalculation_container_hide").hide();
 				jQuery(".smeta_hide").hide();
@@ -2212,7 +2212,7 @@
 				jQuery("#btn_fixtures").click();
 				jQuery("#btn_fixtures").attr("disabled", "disabled");
 			}
-			jQuery(".container").on("change", "#n13_count #ecola_count #n13 .ecola2", function () {
+			jQuery("body").on("change", "#n13_count #ecola_count #n13 .ecola2", function () {
 			//jQuery("#n13_count #ecola_count #n13 .ecola2").change( function () {
 				console.log("ыщвшоаыв");
 				if ((jQuery("#n13_count").val() != null && jQuery("#n13_count").val() != undefined && jQuery("#n13_count").val() != "" && jQuery("#n13_count").val() != 0) || (jQuery("#ecola_count").val() != null && jQuery("#ecola_count").val() != undefined && jQuery("#ecola_count").val() != "" && jQuery("#ecola_count").val() != 0) || (jQuery("#n13").val() != null && jQuery("#n13").val() != undefined && jQuery("#n13").val() != "" && jQuery("#n13").val() != 0) || (jQuery(".ecola2").val() != null && jQuery(".ecola2").val() != undefined && jQuery(".ecola2").val() != "" && jQuery(".ecola2").val() != 0)) {
