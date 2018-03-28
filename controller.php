cf306579887bb05f730e37c51af1cdf7cda32aff
DenchikPushka <void.anonim@gmail.com>
2018-03-28 11:26:52 +0300

    aaaaaa

--- controller.php
+++ controller.php
@@ -2924,11 +2924,12 @@ class Gm_ceilingController extends JControllerLegacy
             $users_model = Gm_ceilingHelpersGm_ceiling::getModel('users');
             $items = $users_model->findDealersByCity('Воронеж');
             $count = 0;
-            //print_r($items);
-            //exit();
+            
             $dop_contacts_model = Gm_ceilingHelpersGm_ceiling::getModel('clients_dop_contacts');
             foreach ($items as $i => $item)
             {
+                print_r($item);
+                exit();
                 $client_id = $item->associated_client;
                 $emails = $dop_contacts_model->getEmailByClientID($client_id);
                 foreach ($emails as $j => $email)
