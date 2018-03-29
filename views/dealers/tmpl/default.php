ff430f2806a4293d21eaebc4a6133cb11d05c394
DenchikPushka <void.anonim@gmail.com>
2018-03-29 16:51:25 +0300

    log

--- views/dealers/tmpl/default.php
+++ views/dealers/tmpl/default.php
@@ -265,7 +265,8 @@ $recoil_map_model = Gm_ceilingHelpersGm_ceiling::getModel('recoil_map_project');
                         if (target.id == 'save_kp')
                         {
                             cleditor.select();
-                            var text = btoa(cleditor[0].selectedHTML().outerHTML);
+                            var text = cleditor[0].selectedHTML();
+                            
                             console.log(text);
                             jQuery.ajax({
                                 type: 'POST',
