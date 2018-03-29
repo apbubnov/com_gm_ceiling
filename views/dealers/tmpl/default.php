ab9777303209f9ff01e6fddc2d0a21d76cd898c5
DenchikPushka <void.anonim@gmail.com>
2018-03-29 10:51:44 +0300

    fixx

--- views/dealers/tmpl/default.php
+++ views/dealers/tmpl/default.php
@@ -98,7 +98,7 @@ $recoil_map_model = Gm_ceilingHelpersGm_ceiling::getModel('recoil_map_project');
 
         jQuery('#dealer_contacts').mask('+7(999) 999-9999');
 
-        var cleditor = $("#input").cleditor();
+        var cleditor = $("#email_text").cleditor();
 
         jQuery("#new_dealer").click(function(){
             jQuery("#close").show();
