a59503d3db39efcd19c10a6297ac85d86e245f95
DenchikPushka <void.anonim@gmail.com>
2018-05-16 13:59:51 +0300

    project views

--- views/project/tmpl/default_calculator_project.php
+++ views/project/tmpl/default_calculator_project.php
@@ -240,9 +240,10 @@ $project_total_discount = $project_total_discount  + $client_sum_transport;
     <!-- конец -->
 
     <script type="text/javascript" src="/components/com_gm_ceiling/create_calculation.js"></script>
-
-    <script>
+    <script type="text/javascript" src="/components/com_gm_ceiling/views/project/common_table.js"></script>
+    <script type="text/javascript">
         var $ = jQuery;
+        var project_id = "<?php echo $this->item->id; ?>";
         jQuery(document).ready(function () {
 
             document.getElementById('add_calc').onclick = function()
--- views/project/tmpl/default_calculator_refused.php
+++ views/project/tmpl/default_calculator_refused.php
@@ -246,8 +246,9 @@ $project_total_discount = $project_total_discount  + $client_sum_transport;
 <?php include_once('components/com_gm_ceiling/views/project/common_table.php'); ?>
 	
 <script type="text/javascript" src="/components/com_gm_ceiling/create_calculation.js"></script>
-
-<script>
+<script type="text/javascript" src="/components/com_gm_ceiling/views/project/common_table.js"></script>
+<script type="text/javascript">
+    var project_id = "<?php echo $this->item->id; ?>";
 	jQuery(document).ready(function(){
 		
         document.getElementById('add_calc').onclick = function()
