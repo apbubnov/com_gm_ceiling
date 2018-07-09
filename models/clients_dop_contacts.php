d2d9f3396a3b8725ec8760950e69f282045c9d99
apbubnov <al.p.bubnov@gmail.com>
2018-07-09 16:20:07 +0300

    change client data in calculator calendar

--- models/clients_dop_contacts.php
+++ models/clients_dop_contacts.php
@@ -32,34 +32,40 @@ class Gm_ceilingModelClients_dop_contacts extends JModelList
 
 	
 	public function save($client_id,$type_id,$contact){
-		$columns = array('client_id', 'type_id', 'contact');
-		$values = "$client_id, $type_id, '$contact'";
+		try{
+			$columns = array('client_id', 'type_id', 'contact');
+			$values = "$client_id, $type_id, '$contact'";
 
-		$db    = JFactory::getDbo();
+			$db    = JFactory::getDbo();
 
-		$query = $db->getQuery(true);
-		$query->select('*');
-		$query->from('`#__gm_ceiling_clients_dop_contacts`');
-		$query->where("`client_id` = $client_id AND `contact` = '$contact'");
-		$db->setQuery($query);
-		$items = $db->loadObjectList();
-
-		if (count($items) == 0)
-		{
 			$query = $db->getQuery(true);
-			$query->insert($db->quoteName('#__gm_ceiling_clients_dop_contacts'));
-			$query->columns($db->quoteName($columns));
-			$query->values($values);
-		
+			$query->select('*');
+			$query->from('`#__gm_ceiling_clients_dop_contacts`');
+			$query->where("`client_id` = $client_id AND `contact` = '$contact'");
 			$db->setQuery($query);
-			$db->execute();
-			$result = $db->insertid();
+			$items = $db->loadObjectList();
+
+			if (count($items) == 0)
+			{
+				$query = $db->getQuery(true);
+				$query->insert($db->quoteName('#__gm_ceiling_clients_dop_contacts'));
+				$query->columns($db->quoteName($columns));
+				$query->values($values);
+			
+				$db->setQuery($query);
+				$db->execute();
+				$result = $db->insertid();
+			}
+			else
+			{
+				$result = $items[0]->id;
+			}
+			return $result;
 		}
-		else
-		{
-			$result = $items[0]->id;
-		}
-		return $result;
+		catch(Exception $e)
+        {
+            Gm_ceilingHelpersGm_ceiling::add_error_in_log($e->getMessage(), __FILE__, __FUNCTION__, func_get_args());
+        }
     }
 
     public function getEmailByClientID($id){
@@ -102,54 +108,90 @@ class Gm_ceilingModelClients_dop_contacts extends JModelList
 	}
 	
 	public function SetEmail($client_id,$contact){
-		//print_r($contact); exit;
-        $db    = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query
-            ->select('id')
-            ->from('#__gm_ceiling_clients_dop_contacts')
-            ->where("client_id = $client_id AND contact = ".$db->quote($contact));
-        $db->setQuery($query);
-        $items = $db->loadObject();
-		//print_r($contact); exit;
-		if(empty($items->id))
-		{
-		$columns = array('client_id', 'type_id', 'contact');
-		//$values = "$client_id, 1 , '$db->quote($contact)'";
+		try{
+			//print_r($contact); exit;
+	        $db    = JFactory::getDbo();
+	        $query = $db->getQuery(true);
+	        $query
+	            ->select('id')
+	            ->from('#__gm_ceiling_clients_dop_contacts')
+	            ->where("client_id = $client_id AND contact = ".$db->quote($contact));
+	        $db->setQuery($query);
+	        $items = $db->loadObject();
+			//print_r($contact); exit;
+			if(empty($items->id))
+			{
+			$columns = array('client_id', 'type_id', 'contact');
+			//$values = "$client_id, 1 , '$db->quote($contact)'";
 
-		$db    = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		$query->insert($db->quoteName('#__gm_ceiling_clients_dop_contacts'));
-		$query->columns($db->quoteName($columns));
-		$query->values(
-			$client_id.','
-			.'1'.','
-			.$db->quote($contact));
-	
-		$db->setQuery($query);
-		$db->execute();
+			$db    = JFactory::getDbo();
+			$query = $db->getQuery(true);
+			$query->insert($db->quoteName('#__gm_ceiling_clients_dop_contacts'));
+			$query->columns($db->quoteName($columns));
+			$query->values(
+				$client_id.','
+				.'1'.','
+				.$db->quote($contact));
+		
+			$db->setQuery($query);
+			$db->execute();
 
+			}
 		}
+		catch(Exception $e)
+        {
+            Gm_ceilingHelpersGm_ceiling::add_error_in_log($e->getMessage(), __FILE__, __FUNCTION__, func_get_args());
+        }
     }
 	public function getContact($id){
-        $db    = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query
-            ->select('contact')
-            ->from('#__gm_ceiling_clients_dop_contacts')
-            ->where("client_id = $id");
-        $db->setQuery($query);
-        $items = $db->loadObjectList();
-        return $items;
+		try {
+	        $db    = JFactory::getDbo();
+	        $query = $db->getQuery(true);
+	        $query
+	            ->select('contact')
+	            ->from('#__gm_ceiling_clients_dop_contacts')
+	            ->where("client_id = $id");
+	        $db->setQuery($query);
+	        $items = $db->loadObjectList();
+	        return $items;
+	    }
+	    catch(Exception $e)
+        {
+            Gm_ceilingHelpersGm_ceiling::add_error_in_log($e->getMessage(), __FILE__, __FUNCTION__, func_get_args());
+        }
 	}
 	public function removeEmail($id,$email){
-		$db    = JFactory::getDbo();
-        $query = $db->getQuery(true);
-        $query->delete('#__gm_ceiling_clients_dop_contacts');
-		$query->where("client_id = $id and contact = '$email'");
-		$db->setQuery($query);
-		$result = $db->execute();
-        return $result;
+		try{
+			$db    = JFactory::getDbo();
+	        $query = $db->getQuery(true);
+	        $query->delete('#__gm_ceiling_clients_dop_contacts');
+			$query->where("client_id = $id and contact = '$email'");
+			$db->setQuery($query);
+			$result = $db->execute();
+	        return $result;
+	    }
+	    catch(Exception $e)
+        {
+            Gm_ceilingHelpersGm_ceiling::add_error_in_log($e->getMessage(), __FILE__, __FUNCTION__, func_get_args());
+        }
+	}
+
+	function updateEmail($client_id,$new_value,$old_value){
+		try{
+			$db = JFactory::getDbo();
+	        $query = $db->getQuery(true);
+	        $query->update('#__gm_ceiling_clients_dop_contacts');
+			$query->set("contact = '$new_value'");
+			$query->where("contact LIKE '%$old_value%' and client_id = $client_id");
+			$db->setQuery($query);
+			$result = $db->execute();
+	        return $result;
+
+		}
+		catch(Exception $e)
+        {
+            Gm_ceilingHelpersGm_ceiling::add_error_in_log($e->getMessage(), __FILE__, __FUNCTION__, func_get_args());
+        }
 	}
 }
 ?>
