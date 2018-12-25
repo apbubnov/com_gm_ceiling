1e3ad591919d59dc144aa2481d6af49a6af664f9
DenchikPushka <void.anonim@gmail.com>
2018-12-25 15:51:42 +0300

    fixx print callbacks

--- views/callback/tmpl/default.php
+++ views/callback/tmpl/default.php
@@ -20,20 +20,12 @@ $user_group = $user->groups;
     <table class="table table-striped one-touch-view g_table" id="callbacksList">
         <thead>
         <tr>
-            <th>
-               ФИО клиента
-            </th>
-            <th>
-               Дата
-            </th>
-            <th>
-               Примечание
-            </th>
-            <th>
-            </th>
+            <th>ФИО клиента</th>
+            <th>Дата</th>
+            <th>Примечание</th>
+            <th></th>
         </tr>
         </thead>
-
         <tbody id="table_body">
         </tbody>
     </table>
@@ -45,40 +37,36 @@ $user_group = $user->groups;
     var arr_calls = [];
     var table_body_elem = document.getElementById('table_body');
     var user_id = <?php echo $userId; ?>;
-    function show_calls()
-    {
-        var str;
+    function show_calls() {
         var calendar_elem_value = document.getElementById('calendar').value;
-        table_body_elem.innerHTML = "";
-        for (var i = 0; i < arr_calls.length; i++)
-        {
-                str = '';
-                if (arr_calls[i].dealer_type == 3)
-                {
-                    str += '<tr data-href="index.php?option=com_gm_ceiling&view=clientcard&type=designer&id='+(arr_calls[i].client_id-0)+'&call_id='+(arr_calls[i].id-0)+'">';
-                }
-                else if (arr_calls[i].dealer_type == 5)
-                {
-                    str += '<tr data-href="index.php?option=com_gm_ceiling&view=clientcard&type=designer2&id='+(arr_calls[i].client_id-0)+'&call_id='+(arr_calls[i].id-0)+'">';
-                }
-                else if (arr_calls[i].dealer_type == 0 || arr_calls[i].dealer_type == 1)
-                {
-                    str += '<tr data-href="index.php?option=com_gm_ceiling&view=clientcard&type=dealer&id='+(arr_calls[i].client_id-0)+'&call_id='+(arr_calls[i].id-0)+'">';
-                }
-                else
-                {
-                    str += '<tr data-href="index.php?option=com_gm_ceiling&view=clientcard&id='+(arr_calls[i].client_id-0)+'&call_id='+(arr_calls[i].id-0)+'">';
-                }
-                str += '<td>'+arr_calls[i].client_name+'</td>';
-                str += '<td>'+arr_calls[i].date_time+'</td>';
-                str += '<td>'+arr_calls[i].comment+'</td>';
-                str += '<td><button class="btn btn-danger btn-sm" type="button" onclick="del_call('+arr_calls[i].id+')"><i class="fa fa-trash" aria-hidden="true"></i></button></td>';
-                str += '</tr>';
-                table_body_elem.innerHTML += str;
+        table_body_elem.innerHTML = '';
+        for (var i = 0; i < arr_calls.length; i++) {
+            setTimeout(printTr, 0, arr_calls[i]);
+        }
+
+        function printTr(arr_calls_i) {
+            var str, tr, td;
+            if (arr_calls_i.dealer_type !== null) {
+                arr_calls_i.dealer_type = arr_calls_i.dealer_type-0;
+            }
+            tr = table_body_elem.insertRow();
+            tr.setAttribute('data-callId', arr_calls_i.id-0);
+            tr.setAttribute('data-dealerType', arr_calls_i.dealer_type);
+            tr.setAttribute('data-clientId', arr_calls_i.client_id-0);
+            td = tr.insertCell();
+            td.innerHTML = arr_calls_i.client_name;
+            td = tr.insertCell();
+            td.innerHTML = arr_calls_i.date_time;
+            td = tr.insertCell();
+            td.innerHTML = arr_calls_i.comment;
+            td = tr.insertCell();
+            td.innerHTML = '<button class="btn btn-danger btn-sm" type="button" data-id="'+arr_calls_i.id+'"><i class="fa fa-trash" aria-hidden="true"></i></button>';
+            td.getElementsByTagName('button')[0].onclick = del_call;
+            tr.onclick = clickTr;
         }
     }
 
-    function getData(){
+    function getData() {
         jQuery.ajax({
             type: 'POST',
             url: "index.php?option=com_gm_ceiling&task=callback.getData",
@@ -87,7 +75,6 @@ $user_group = $user->groups;
                 date: jQuery('#calendar').val()
             },
             success: function(data){
-                console.log(12312);
                 arr_calls = data;
                 show_calls();
             },
@@ -105,27 +92,35 @@ $user_group = $user->groups;
             }
         });
     }
-    jQuery(document).ready(function()
-    {
 
+    function clickTr(e) {
+        if (this.getAttribute('data-clientId') && !del_click_bool) {
+            var type = this.getAttribute('data-dealerType')-0;
+            var url;
+            if (type === 3) {
+                url = 'index.php?option=com_gm_ceiling&view=clientcard&type=designer&id=';
+            } else if (type === 5) {
+                url = 'index.php?option=com_gm_ceiling&view=clientcard&type=designer2&id=';
+            } else if (type === 0 || type === 1) {
+                url = 'index.php?option=com_gm_ceiling&view=clientcard&type=dealer&id=';
+            } else {
+                url = 'index.php?option=com_gm_ceiling&view=clientcard&id=';
+            }
+            document.location.href = url+(this.getAttribute('data-clientId')-0)+'&call_id='+(this.getAttribute('data-callId')-0);
+        }
+        del_click_bool = false;
+    }
+
+    jQuery(document).ready(function() {
         getData();
 
         document.getElementById('calendar').onchange = function(){
             getData();
         };
-
-        jQuery('body').on('click', 'tr', function(e)
-        {
-            if (jQuery(this).data('href') !== undefined && !del_click_bool)
-            {
-                document.location.href = jQuery(this).data('href');
-            }
-            del_click_bool = false;
-        });
     });
 
-    function del_call(id)
-    {
+    function del_call() {
+        var id = this.getAttribute('data-id')-0;
         del_click_bool = true;
 
         jQuery.ajax({
@@ -135,10 +130,8 @@ $user_group = $user->groups;
                 call_id: id
             },
             success: function(data){
-                for (var i = arr_calls.length; i--;)
-                {
-                    if (arr_calls[i].id == id)
-                    {
+                for (var i = arr_calls.length; i--;) {
+                    if (arr_calls[i].id == id) {
                         arr_calls.splice(i, 1);
                     }
                 }
